565620541060c0680f2a697c6c6cf3f784f3179c
marambeddouihech <beddouihechmaram05@gmail.com>
2025-02-26 15:38:49 +0100

    Update api.py

--- R9/api.py
+++ R9/api.py
@@ -1,6 +1,6 @@
 import requests
 import logging
-from private_information import RAISON_SERVICE_URL, NASSIM_API_URL
+from private_information import RAISON_SERVICE_URL
 
 # Configuration des logs
 logging.basicConfig(level=logging.INFO)
