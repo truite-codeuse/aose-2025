11b1b8b82224e35279a0607e463bee0d8cac8ce1
marambeddouihech <beddouihechmaram05@gmail.com>
2025-02-26 15:31:05 +0100

    Create private_info.py

---
+++ R9/private_info.py
@@ -0,0 +1,5 @@
+# URLs des services externes
+RAISON_SERVICE_URL = "http://127.0.0.1:8004"  # URL de l'agent d'argumentation (R8)
+
+# Clés API 
+API_KEY = "gHWO8Hpd2LPSLkCyseUZ836NS0LtHGEFhyxIrKj0"
